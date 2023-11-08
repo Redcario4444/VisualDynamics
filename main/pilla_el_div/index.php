0de8a2778500ef5601f386b2602ae73408b08d5a
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-08 11:56:33 +0100

    Seguimos trabajando

---
+++ main/pilla_el_div/index.php
@@ -0,0 +1,16 @@
+<?php
+    $descripcion = ""; /*TODO AÑADIR DESCRIPCIÓN DE LA PÁGINA*/
+    $titulo = ""; /*TODO AÑADIR TÍTULO DE LA PÁGINA*/
+    $css = ""; /*TODO AÑADIR LINK AL CSS DE ESTA PÁGINA*/
+    $js = ""; /*TODO AÑADIR LINK AL JS DE ESTA PÁGINA*/
+    include "templates/header.php"; #TODO COMPROBAR RUTA!
+?>
+
+
+<div id="campo">
+</div>
+<audio id="rebote" src="/assets/sound/pilla_el_div/rebote.mp3"></audio>
+
+<?php
+    include 'templates/footer.php'; #TODO COMPROBAR RUTA!
+?>
