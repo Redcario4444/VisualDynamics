8fce443c54d50a51a374e6662cb2668e76d14f51
Víctor Hell <victorhellinsaez@gmail.com>
2023-11-10 00:15:47 +0100

    Avence con pilla el Div

--- main/pilla_el_div/index.php
+++ main/pilla_el_div/index.php
@@ -1,5 +1,5 @@
 <?php
-    $descripcion = "Pilla el Div, un juego que comprueba tus reflejos y movimientos rápidos";
+    $descripcion = "Pilla el Div, un juego 5r que comprueba tus reflejos y movimientos rápidos";
     $titulo = "Pilla el Div";
     $css = "../../src/css/header_footer.css";
     $css_propio = "../../src/css/pilla_el_div.css";
@@ -8,11 +8,22 @@
     $ruta_canciones = '../../';
     include "../../src/header.php";
 ?>
-<h1>Pilla el Div</h1>
-
-<div id="campo">
+<div class="seleccion">
+    <ul>
+        <li class="botones" id='facil'>Fácil</li>
+        <li class='botones' id='normal'>Normal</li>
+        <li class='botones' id='dificil'>Difícil</li>
+        <li class='botones' id='infinito'>Infinito</li>
+    </ul>
+</div>
+<div id="campo" class="oculto campo">
+    <div id='score'>
+        <p>Score: <span id='score-number'>0</span></p>
+    </div>
 </div>
 <audio id="rebote" src="/assets/sound/pilla_el_div/rebote.mp3"></audio>
+<audio id="bienvenida" src="/assets/sound/pilla_el_div/welcome.wav"></audio>
+<audio id="dificil" src="/assets/sound/pilla_el_div/dificil.wav"></audio>
 
 <?php
     include '../../src/footer.php';
--- src/js/pilla_el_div.js
+++ src/js/pilla_el_div.js
@@ -1,60 +1,101 @@
 document.addEventListener('DOMContentLoaded', function () {
     let container = document.getElementById('campo');
-    let rebote = document.getElementById("rebote");
-    let divCount = 4;
-    let divs = [];
-    let campoRect = container.getBoundingClientRect();
-    let campoX = campoRect.left;
-    let campoY = campoRect.top;
+    let botones = document.getElementsByClassName('botones');
+    let seleccion = document.getElementsByClassName('seleccion')[0];
+    let score = 0;
+    let scoreNumber = document.getElementById('score-number');
 
-    for (let i = 0; i < divCount; i++) {
-        let div = document.createElement('div');
-        div.className = 'reboteDiv';
-        container.appendChild(div);
+    // Agregar el escuchador de clic al contenedor una sola vez
+    container.addEventListener('click', function (event) {
+        // Verificar si el objetivo del clic tiene la clase 'reboteDiv'
+        if (event.target.classList.contains('reboteDiv')) {
+            // Eliminar el elemento clicado
+            event.target.remove();
+            score++;
+            scoreNumber.textContent = score;
+            // Llamar a la función dificultad con 1 div y la velocidad
+            dificultad(1, velocidad);
+        }
+    });
 
-        let fixedSpeed = 5;
+    for (let i = 0; i < botones.length; i++) {
+        botones[i].addEventListener('click', function () {
+            seleccion.classList.add('oculto');
+            container.classList.remove('oculto');
+            if (botones[i].id === 'facil') {
+                container.classList.add('fondo-facil');
 
-        let angle = Math.random() * Math.PI * 2;
-        let dx = Math.cos(angle) * fixedSpeed;
-        let dy = Math.sin(angle) * fixedSpeed;
+                setTimeout(function () {
+                    dificultad(numeroDeDivs = 5, velocidad = 3);
+                }, 1000);
 
-        const x = Math.random() * campoRect.width;
-        const y = Math.random() * campoRect.height;
-
-        divs.push({
-            element: div,
-            x,
-            y,
-            dx,
-            dy,
-            maxX: campoRect.width - div.clientWidth +10,
-            maxY: campoRect.height - div.clientHeight +10
+            } else if (botones[i].id === 'normal') {
+                container.classList.add('fondo-normal');
+            } else if (botones[i].id === 'dificil') {
+                container.classList.add('fondo-dificil');
+            } else if (botones[i].id === 'infinito') {
+                container.classList.add('fondo-infinito');
+            }
         });
-    }
 
-    function animarRebote() {
-        divs.forEach((divData) => {
-            const { element, x, y, dx, dy, maxX, maxY } = divData;
-            let newX = x + dx;
-            let newY = y + dy;
+        function dificultad(divCount, velocidad) {
+            const arrayDivs = [];
+            const campoJuego = container.getBoundingClientRect();
+            const campoHorizontal = campoJuego.left;
+            const campoVertical = campoJuego.top;
 
-            if (newX < 0 || newX > maxX) {
-                divData.dx = -dx;
-                rebote.play();
-            }else if (newY < 0 || newY > maxY) {
-                divData.dy = -dy;
-                rebote.play();
+            for (let i = 0; i < divCount; i++) {
+                const div = document.createElement('div');
+                div.className = 'reboteDiv';
+                container.appendChild(div);
+
+                // Velocidad fija (puedes ajustarla)
+                fixedSpeed = velocidad;
+
+                // Direcciones aleatorias
+                const angulo = Math.random() * Math.PI * 2;
+                const velocidadHorizontal = Math.cos(angulo) * fixedSpeed;
+                const velocidadVertical = Math.sin(angulo) * fixedSpeed;
+
+                // Posiciones aleatorias dentro de "campo" teniendo en cuenta el tamaño del div
+                const generacionHorizontal = Math.random() * (campoJuego.width - div.clientWidth);
+                const generacionVertical = Math.random() * (campoJuego.height - div.clientHeight);
+
+                arrayDivs.push({
+                    element: div,
+                    x: generacionHorizontal,
+                    y: generacionVertical,
+                    dx: velocidadHorizontal,
+                    dy: velocidadVertical,
+                    maxX: campoJuego.width - div.clientWidth,
+                    maxY: campoJuego.height - div.clientHeight,
+                });
             }
 
-            element.style.left = (campoX + newX) + 'px';
-            element.style.top = (campoY + newY) + 'px';
+            function animarRebote() {
+                arrayDivs.forEach((divData) => {
+                    const { element, x, y, dx, dy, maxX, maxY } = divData;
+                    let newX = x + dx;
+                    let newY = y + dy;
 
-            divData.x = newX;
-            divData.y = newY;
-        });
+                    if (newX < 0 || newX > maxX) {
+                        divData.dx = -dx;
+                    }
 
-        requestAnimationFrame(animarRebote);
+                    if (newY < 0 || newY > maxY) {
+                        divData.dy = -dy;
+                    }
+
+                    element.style.left = (campoHorizontal + newX) + 'px';
+                    element.style.top = (campoVertical + newY) + 'px';
+
+                    divData.x = newX;
+                    divData.y = newY;
+                });
+
+                requestAnimationFrame(animarRebote);
+            }
+            animarRebote();
+        }
     }
-
-    animarRebote();
 });
