a973289ddaa4622143e6ac40212d5dd37bc4ad21
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-11-11 16:19:30 +0100

    Continuamos trabajando.

--- main/memory_cards/index.php
+++ main/memory_cards/index.php
@@ -13,50 +13,77 @@
     <div class="container">
         <!-- Menú de Selección de Dificultad -->
         <div id='menu-dificultad'>
-            <button class="botones-seleccion" id='facil'>Fácil
-                <span class='tooltip-text'>Dif. Fácil con 'f'</span></button>
-            <button class='botones-seleccion' id='medio'>Medio
-                <span class='tooltip-text'>Dif. Media con 'm'</span></button>
-            <button class='botones-seleccion' id='dificil'>Difícil
-                <span class='tooltip-text'>Max. Dif. con 'd'</span></button>
+            <button class="botones-seleccion dificultad" id='facil'>Fácil
+                <span class='tooltip-text tooltip-top'>Seleccióname con la tecla 'f'</span></button>
+            <button class='botones-seleccion dificultad' id='medio'>Medio
+                <span class='tooltip-text  tooltip-top'>Seleccióname con la tecla 'm'</span></button>
+            <button class='botones-seleccion dificultad' id='dificil'>Difícil
+                <span class='tooltip-text  tooltip-top'>Seleccióname con la tecla 'd'</span></button>
         </div>
 
-        <button class="botones-seleccion marcador">Score: <span id="score"></span></button>
-        <button class='botones-seleccion marcador'>Clicks: <span id='clicks'></span></button>
-        <button class='botones-seleccion marcador'>Tiempo: <span id='tiempo'></span></button>
+        <!-- Menú de puntuación de usuario -->
 
-        <button class='botones-seleccion' id='reiniciar'>Reiniciar
-            <span class="tooltip-text">Reinicia con 'r'</span></button>
+        <button class="botones-seleccion marcador">Score:
+            <span id="score"></span>
+            <span class="tooltip-text tooltip-bottom">¡Intenta conseguir la mayor puntuación!</span>
+        </button>
+        <button class='botones-seleccion marcador'>Clicks:
+            <span id='clicks'></span>
+            <span class="tooltip-text tooltip-bottom">¡Intenta hacer la menor cantidad de clicks posible!</span>
+        </button>
+        <button class='botones-seleccion marcador'>Tiempo:
+            <span id='tiempo'></span>
+            <span class='tooltip-text tooltip-bottom'>¡Intenta resolverlo en el menor tiempo posible!</span>
+        </button>
 
-        <button class="botones-seleccion" id='boton-fondo'>Elegir fondo
-            <span class='tooltip-text'>Elige con 'e'</span></button>
+        <!-- Botón de reinicio -->
+        <button class='botones-seleccion reiniciar' id='reiniciar'>Reiniciar
+            <span class="tooltip-text tooltip-top">Seleccióname con la tecla 'r'</span></button>
 
-        <!-- Modal de selección de fondo -->
+        <!-- Menú de selección de fondos -->
+        <button class="botones-seleccion seleccion-fondo" id='boton-fondo'>Elegir fondo
+            <span class='tooltip-text  tooltip-top'>Seleccióname con la tecla 'e'</span></button>
+
+        <!-- Modal de selección de fondos -->
         <div class='fondo-modal' id='fondo-modal'></div>
-        <div id='modal-fondos'>
+        <div class='modal-fondos' id="modal-fondos">
             <div class="interior-modal">
-                <div id='seleccion-colores'>
+                <div class='seleccion-colores' id="seleccion-colores">
                     <p>Elige un color:</p>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #000000;'></div>
+                        <div class='color' style='background-color: #000000;'>
+                            <span class='tooltip-text tooltip-bottom'>NEGRO</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #00ff00;'></div>
+                        <div class='color' style='background-color: #00ff00;'>
+                            <span class="tooltip-text tooltip-bottom">VERDE</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #0000ff;'></div>
+                        <div class='color' style='background-color: #0000ff;'>
+                            <span class="tooltip-text tooltip-bottom">AZUL</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #00ffff;'></div>
+                        <div class='color' style='background-color: #00ffff;'>
+                            <span class='tooltip-text tooltip-bottom'>CYAN</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #ff0000;'></div>
+                        <div class='color' style='background-color: #ff0000;'>
+                            <span class='tooltip-text tooltip-bottom'>ROJO</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #ff00ff;'></div>
+                        <div class='color' style='background-color: #ff00ff;'>
+                            <span class='tooltip-text tooltip-bottom'>MAGENTA</span>
+                        </div>
                     </div>
                     <div class='color-option'>
-                        <div class='color' style='background-color: #ffff00;'></div>
+                        <div class='color' style='background-color: #ffff00;'>
+                            <span class='tooltip-text tooltip-bottom'>AMARILLO</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -64,36 +91,36 @@
                 <div class='objetoSVG'>
                     <object id='fondo0' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo0.svg'></object>
-                    <span class="tooltip-text">DEFAULT</span>
+                    <span class="tooltip-text tooltip-top">DEFAULT</span>
                 </div>
                 <div class='objetoSVG'>
                     <object id='fondo1' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo1.svg'></object>
-                    <span class='tooltip-text'>CHINA</span>
+                    <span class='tooltip-text tooltip-top'>JAPÓN</span>
                 </div>
                 <div class='objetoSVG'>
                     <object id='fondo2' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo2.svg'></object>
-                    <span class='tooltip-text'>ÁFRICA</span>
+                    <span class='tooltip-text tooltip-top'>ÁFRICA</span>
                 </div>
                 <div class='objetoSVG'>
                     <object id='fondo3' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo3.svg'></object>
-                    <span class='tooltip-text'>PÍXEL</span>
+                    <span class='tooltip-text tooltip-top'>MOROCCO</span>
                 </div>
                 <div class='objetoSVG'>
                     <object id='fondo4' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo4.svg'></object>
-                    <span class='tooltip-text'>FLOR</span>
+                    <span class='tooltip-text tooltip-top'>ESPAÑA</span>
                 </div>
                 <div class='objetoSVG'>
                     <object id='fondo5' type='image/svg+xml'
                             data='../../assets/img/memory_cards/fondos/fondo5.svg'></object>
-                    <span class='tooltip-text'>POKÉ<br>BALL</span>
+                    <span class='tooltip-text tooltip-top'>KANTO</span>
                 </div>
             </div>
-            <button class=" botones-seleccion
-                " id='cerrar-modal'>Cerrar
+            <button class="botones-seleccion" id='cerrar-modal'>Cerrar
+                <span class='tooltip-text  tooltip-right'>Ciérrame con 'c'</span>
             </button>
         </div>
     </div>
