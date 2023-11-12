e003ea7deb0a2454ff63fe9f7ae95ec7eeccc8e0
Víctor Hell <victorhellinsaez@gmail.com>
2023-11-12 20:57:27 +0100

    Hundir la flota ya bastante bien

--- src/js/hundir_la_flota.js
+++ src/js/hundir_la_flota.js
@@ -1,120 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const p1Campo = document.getElementById('p1-campo');
-    const p1Disparos = document.getElementById('p1-disparos');
-    const p2Campo = document.getElementById('p2-campo');
-    const p2Disparos = document.getElementById('p2-disparos');
-
-    function crearCampoDeJuego(contenedor) {
-        for (let i = 0; i < 100; i++) {
-            const celda = document.createElement('div');
-            celda.className = 'celda';
-            celda.dataset.row = Math.floor(i / 10);
-            celda.dataset.col = i % 10;
-            contenedor.appendChild(celda);
-        }
-    }
-
-    function colocarBarcos(contenedor) {
-        const ships = [
-            {length: 4, count: 1},
-            {length: 3, count: 2},
-            {length: 2, count: 3},
-            {length: 1, count: 4}
-        ];
-
-        ships.forEach((ship) => {
-            for (let i = 0; i < ship.count; i++) {
-                colocarBarco(contenedor, ship.length);
-            }
-        });
-    }
-
-    function colocarBarco(contenedor, length) {
-        let position;
-        let attempt = 0;
-        const maxAttempts = 100;
-
-        do {
-            position = getRandomPosition(length);
-            attempt++;
-        } while ((!checkSurroundingCells(contenedor, position) || isOverlap(contenedor, position)) && attempt < maxAttempts);
-
-        if (attempt >= maxAttempts) {
-            console.log("No se pudo colocar el barco después de varios intentos.");
-            return;
-        }
-
-        // Marcar las celdas ocupadas por el barco
-        markShipCells(contenedor, position, length);
-    }
-
-    function getRandomPosition(length) {
-        const direction = Math.random() < 0.5 ? 'horizontal' : 'vertical';
-        const buffer = 1;
-        let row, col;
-
-        if (direction === 'horizontal') {
-            row = Math.floor(Math.random() * 10);
-            col = Math.floor(Math.random() * (10 - length - buffer));
-        } else {
-            row = Math.floor(Math.random() * (10 - length - buffer));
-            col = Math.floor(Math.random() * 10);
-        }
-
-        return {
-            row,
-            col,
-            direction,
-            length
-        };
-    }
-
-    function checkSurroundingCells(contenedor, position) {
-        const buffer = 1;
-        const endRow = position.direction === 'vertical' ? position.row + position.length : position.row;
-        const endCol = position.direction === 'horizontal' ? position.col + position.length : position.col;
-
-        for (let i = position.row - buffer; i <= endRow + buffer; i++) {
-            for (let j = position.col - buffer; j <= endCol + buffer; j++) {
-                if (i < 0 || i >= 10 || j < 0 || j >= 10) {
-                    continue;
-                }
-
-                const cell = contenedor.querySelector(`[data-row="${i}"][data-col="${j}"]`);
-                if (cell && cell.classList.contains('ship')) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    function isOverlap(contenedor, position) {
-        for (let i = 0; i < position.length; i++) {
-            const checkRow = position.row + (position.direction === 'vertical' ? i : 0);
-            const checkCol = position.col + (position.direction === 'horizontal' ? i : 0);
-
-            const cell = contenedor.querySelector(`[data-row="${checkRow}"][data-col="${checkCol}"]`);
-            if (cell && cell.classList.contains('ship')) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    function markShipCells(contenedor, position, length) {
-        for (let i = 0; i < length; i++) {
-            const checkRow = position.row + (position.direction === 'vertical' ? i : 0);
-            const checkCol = position.col + (position.direction === 'horizontal' ? i : 0);
-
-            const cell = contenedor.querySelector(`[data-row="${checkRow}"][data-col="${checkCol}"]`);
-            if (cell) {
-                cell.classList.add('ship');
-            }
-        }
-    }
+    const p1Campo = document.getElementById('p1Campo');
+    const p1Disparos = document.getElementById('p1Disparos');
+    const p2Campo = document.getElementById('p2Campo');
+    const p2Disparos = document.getElementById('p2Disparos');
+    let contadorDeBarcos = 0;
 
     crearCampoDeJuego(p1Campo);
     colocarBarcos(p1Campo);
@@ -123,4 +12,197 @@ document.addEventListener('DOMContentLoaded', function () {
     crearCampoDeJuego(p2Campo);
     colocarBarcos(p2Campo);
     crearCampoDeJuego(p2Disparos);
+
+    let celdasP1Campo = p1Campo.querySelectorAll('.celda');
+    let celdasP1Disparos = p1Disparos.querySelectorAll('.celda');
+    let celdasP2Campo = p2Campo.querySelectorAll('.celda');
+    let celdasP2Disparos = p1Disparos.querySelectorAll('.celda');
+    let clic = 0;
+
+    p1Ataque(celdasP2Disparos, celdasP2Campo);
+
+    /*cambiarJugador();
+
+    function cambiarJugador() {
+        clic++;
+        if (clic %2 === 0){
+            p1Ataque(celdasP1Disparos, celdasP2Campo);
+        }else {
+            p1Ataque(celdasP2Disparos, celdasP1Campo);
+        }
+    }*/
+
+    function p1Ataque(campoAtaque, campoAtacado){
+        for (let i = 0; i < campoAtaque.length; i++) {
+            campoAtaque[i].addEventListener('click', function (event) {
+                let fila = event.target.dataset.row;
+                let columna = event.target.dataset.col;
+                if (event.target.classList.contains('celda')) {
+                    fila = event.target.dataset.row;
+                    columna = event.target.dataset.col;
+                    event.target.classList.remove('celda');
+                    conseguirMismaCasilla(campoAtaque ,campoAtacado, fila, columna);
+                }else {
+                    alert('Ya habias tocado esta casilla');
+                }
+            });
+        }
+    }
+
+    function conseguirMismaCasilla(campoAtaque ,campoAtacado, fila, columna){
+        let sinTocar = 0;
+        let probarHundirse;
+        for (let i = 0; i < campoAtaque.length; i++) {
+            if (campoAtacado[i].dataset.row === fila && campoAtacado[i].dataset.col === columna){
+                probarHundirse = campoAtacado[i].dataset.barco;
+                if (campoAtacado[i].classList.contains('barco')){
+                    campoAtacado[i].classList.remove('barco');
+                    campoAtacado[i].classList.add('tocado');
+                }else{
+                    campoAtacado[i].classList.add('agua');
+                    /*cambiarJugador();*/
+                    return;
+                }
+            }
+        }
+        for (let i = 0; i < campoAtacado.length; i++) {
+            if (campoAtacado[i].dataset.barco === probarHundirse && !campoAtacado[i].classList.contains('tocado')){
+                sinTocar++;
+            }
+        }
+        if (sinTocar > 0){
+            tocado(campoAtaque, fila, columna);
+        }else {
+            for (let i = 0; i < campoAtacado.length; i++) {
+                if (campoAtacado[i].dataset.barco === probarHundirse && campoAtacado[i].classList.contains('tocado')){
+                    campoAtacado[i].classList.remove('tocado');
+                    campoAtacado[i].classList.add('hundido');
+                    campoAtaque[i].classList.remove('tocado');
+                    campoAtaque[i].classList.add('hundido');
+                }
+            }
+        }
+    }
+
+    function tocado(campoAtaque, fila, columna){
+        for (let i = 0; i < campoAtaque.length; i++) {
+            if (campoAtaque[i].dataset.row === fila && campoAtaque[i].dataset.col === columna){
+                campoAtaque[i].classList.add('tocado');
+            }
+        }
+    }
+
+    function crearCampoDeJuego(contenedor) {
+        for (let i = 0; i < 100; i++) {
+            let celda = document.createElement('div');
+            celda.className = 'celda';
+            celda.dataset.row = Math.floor(i / 10);
+            celda.dataset.col = i % 10;
+            contenedor.appendChild(celda);
+        }
+    }
+
+    function colocarBarcos(contenedor) {
+        let barcos = [
+            {length: 4, count: 1},
+            {length: 3, count: 2},
+            {length: 2, count: 3},
+            {length: 1, count: 4}
+        ];
+
+        barcos.forEach((barco) => {
+            for (let i = 0; i < barco.count; i++) {
+                colocarBarco(contenedor, barco.length);
+            }
+        });
+    }
+
+    function colocarBarco(contenedor, longitud) {
+        let posicion;
+        let intento = 0;
+        let maximoIntentos = 100;
+        contadorDeBarcos++;
+
+        do {
+            posicion = cogerPosicionAleatoria(longitud);
+            intento++;
+        } while ((!comprobarCeldasAdyacentes(contenedor, posicion) || conprobarSuperposicion(contenedor, posicion)) && intento < maximoIntentos);
+
+        if (intento >= maximoIntentos) {
+            console.log("No se pudo colocar el barco después de varios intentos.");
+            return;
+        }
+
+        // Marcar las celdas ocupadas por el barco
+        marcarCeldasBarcos(contenedor, posicion, longitud, contadorDeBarcos);
+    }
+
+    function cogerPosicionAleatoria(longitud) {
+        let direccion = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+        let aux = 1;
+        let fila, columna;
+
+        if (direccion === 'horizontal') {
+            fila = Math.floor(Math.random() * 10);
+            columna = Math.floor(Math.random() * (10 - longitud - aux));
+        } else {
+            fila = Math.floor(Math.random() * (10 - longitud - aux));
+            columna = Math.floor(Math.random() * 10);
+        }
+
+        return {
+            row: fila,
+            col: columna,
+            direction: direccion,
+            length: longitud
+        };
+    }
+
+    function comprobarCeldasAdyacentes(contenedor, posicion) {
+        let aux = 1;
+        let finalFila = posicion.direction === 'vertical' ? posicion.row + posicion.length : posicion.row;
+        let finalColumna = posicion.direction === 'horizontal' ? posicion.col + posicion.length : posicion.col;
+
+        for (let i = posicion.row - aux; i <= finalFila + aux; i++) {
+            for (let j = posicion.col - aux; j <= finalColumna + aux; j++) {
+                if (i < 0 || i >= 10 || j < 0 || j >= 10) {
+                    continue;
+                }
+
+                let celda = contenedor.querySelector(`[data-row="${i}"][data-col="${j}"]`);
+                if (celda && celda.classList.contains('barco')) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    function conprobarSuperposicion(contenedor, posicion) {
+        for (let i = 0; i < posicion.length; i++) {
+            let comprobarfila = posicion.row + (posicion.direction === 'vertical' ? i : 0);
+            let comprobarColumna = posicion.col + (posicion.direction === 'horizontal' ? i : 0);
+
+            let celda = contenedor.querySelector(`[data-row="${comprobarfila}"][data-col="${comprobarColumna}"]`);
+            if (celda && celda.classList.contains('barco')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function marcarCeldasBarcos(contenedor, posicion, longitud, contadorDeBarcos) {
+        for (let i = 0; i < longitud; i++) {
+            let comprobarfila = posicion.row + (posicion.direction === 'vertical' ? i : 0);
+            let comprobarColumna = posicion.col + (posicion.direction === 'horizontal' ? i : 0);
+
+            let celda = contenedor.querySelector(`[data-row="${comprobarfila}"][data-col="${comprobarColumna}"]`);
+            if (celda) {
+                celda.classList.add('barco');
+                celda.dataset.barco = contadorDeBarcos;
+            }
+        }
+    }
 });
